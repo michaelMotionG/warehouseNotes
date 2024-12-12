014766d33d1a87815f0e702e0ae4c100e0ca5123
michaelMotionG <michael.huang@motionG.ai>
2024-12-12 11:01:22 +0800

    add test

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\hp\\testDocus\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\hp\\testDocus\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\hp\\testDocus\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\hp\\testDocus\\my-website\\src\\css\\custom.css"),
+];
